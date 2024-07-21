8de5625f953b25cda4dda5b4832f5b9fa97312d5
Nitin Budhlakoti <113322720+Codebreaker42@users.noreply.github.com>
2024-07-21 23:29:02 +0530

    adding tenstack package

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -6,5 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server:{
     port:8000, // changing the port number
+    proxy:{
+      "/api":{
+        target: "http://localhost:5000",
+        changeOrigin:true,
+      }
+    }
   }
 })
