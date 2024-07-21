0e6fc227bdb2343c52a1a08e90481ef6d042350d
Nitin Budhlakoti <113322720+Codebreaker42@users.noreply.github.com>
2024-07-21 18:43:34 +0530

    setup the front end ui part with react tailwind daisyui

--- front_end/tailwind.config.js
+++ front_end/tailwind.config.js
@@ -1,12 +1,25 @@
+// tailwind.config.js
+import daisyui from "daisyui";
+import daisyUIThemes from "daisyui/src/theming/themes";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [daisyui],
 
+	// daisyui: {
+	// 	themes: [
+	// 		"light",
+	// 		{
+	// 			black: {
+	// 				...daisyUIThemes["[data-theme=black]"],
+	// 				primary: "rgb(29, 155, 240)",
+	// 				secondary: "rgb(24, 24, 24)",
+	// 			},
+	// 		},
+	// 	],
+	// },
+};
